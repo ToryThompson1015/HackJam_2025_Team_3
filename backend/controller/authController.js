a12bd2b1dbc3ec03caf70124376af5a37a524f89
padmaja-ramesh <padmaja-ramesh@users.noreply.github.com>
2025-07-12 16:36:36 -0400

    roles updated

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -26,6 +26,7 @@ const register = async (req, res) => {
       .status(201)
       .json({ message: "User registered successfully", userId: user._id });
   } catch (error) {
+    console.log('Register error:', error);
     res.status(500).json({ message: "Server error" });
   }
 };
